a828fcd8424103ddfd363fe1dfc3a24d1b7dae55
ydy1412 <ydy1412@gmail.com>
2020-10-07 17:20:33 +0900

    local db test

--- Local_Click_House_DB_Context.py
+++ Local_Click_House_DB_Context.py
@@ -13,15 +13,28 @@ class Local_Click_House_DB_Context :
         self.Local_Clickhouse_Id = Local_CLickhouse_Id
         self.Local_Clickhouse_password = Local_Clickhouse_password
         self.DB_NAME = DB_NAME
-        self.Local_Click_House_Engine = create_engine('clickhouse://default@localhost/{0}'.format(self.DB_NAME))
+        self.connect_local_db()
+
+    def connect_local_db(self) :
+        self.Local_Click_House_Engine = create_engine('clickhouse://{0}:{1}@localhost/{2}'.format(self.Local_Clickhouse_Id, self.Local_Clickhouse_password, self.DB_NAME))
         self.Local_Click_House_Conn = self.Local_Click_House_Engine.connect()
-        sql = """
-        SHOW TABLES from {0}
-        """.format(self.DB_NAME)
-        sql_text = text(sql)
-        self.sql_result = pd.read_sql_query(sql_text, self.Local_Click_House_Conn)
+        return True
 
-
+    def create_table(self, table_name) : 
+        
+    def check_table_name(self,table_name ) : 
+        self.connect_local_db()
+        check_table_name_sql = """
+            SHOW TABLES FROM {1}
+            WHERE name = {0}
+        """.format(table_name, self.DB_NAME)
+        sql_text = text(check_table_name_sql)
+        sql_result = pd.read_sql(sql_text, self.Local_Click_House_Conn)
+        if sql_result.shape[0] == 1:
+            return True
+        else :
+            return False
+        
 if __name__ == "__main__" :
-    test_context = Local_Click_House_DB_Context("test","test","test")
-    print(test_context.sql_result)
+    test_context = Local_Click_House_DB_Context("click_house_test1","0000","TEST")
+    print(test_context.check_table_name("TEST"))
