16b1330f56963f8017181f24b2a7240a0ea3c918
Yang <ydy1412@gmail.com>
2020-10-19 16:30:31 +0900

    shop_Data extractor.py

--- Shop_Data_Extractor.py
+++ Shop_Data_Extractor.py
@@ -12,15 +12,23 @@ from dateutil.tz import tzlocal
 from logger import Logger
 
 class Shop_Data_Extractor :
-    def __init__(self, maria_id, maria_password):
+    def __init__(self, maria_id, maria_password, local_clickhouse_id,
+                                    local_clickhouse_password,
+                 local_clickhouse_db_name ):
 
         self.Maria_id = maria_id
         self.Maria_password = maria_password
+
+        self.Clickhouse_id = local_clickhouse_id
+        self.Clickhouse_password = local_clickhouse_password
+        self.Clichouse_DB = local_clickhouse_db_name
+
         local_tz = tzlocal()
         now_time = datetime.now(tz=local_tz).strftime("%Y%m%d%H")
         print(now_time)
         self.inner_logger = Logger(now_time, "shop_data_context_"+now_time+'.json')
         self.connect_db()
+        self.connect_local_clickhouse_db()
 
     def connect_db(self):
         self.MariaDB_Engine = create_engine('mysql+pymysql://{0}:{1}@192.168.100.106:3306/dreamsearch'
@@ -28,11 +36,10 @@ class Shop_Data_Extractor :
         self.MariaDB_Engine_Conn = self.MariaDB_Engine.connect()
         return True
 
-    def connect_local_clickhouse_db(self, local_clickhouse_id,
-                                    local_clickhouse_password, local_clickhouse_db_name):
+    def connect_local_clickhouse_db(self):
         self.Local_Click_House_Engine = create_engine(
-            'clickhouse://{0}:{1}@localhost/{2}'.format(local_clickhouse_id, local_clickhouse_password,
-                                                        local_clickhouse_db_name))
+            'clickhouse://{0}:{1}@localhost/{2}'.format(self.Clickhouse_id, self.Clickhouse_password,
+                                                        self.Clichouse_DB))
         self.Local_Click_House_Conn = self.Local_Click_House_Engine.connect()
         return True
 
@@ -69,7 +76,7 @@ class Shop_Data_Extractor :
         self.product_cate_info_df = pd.read_sql(text_sql, self.MariaDB_Engine_Conn)
         return True
 
-    def extract_product_price_info(self, ADVER_ID_LIST):
+    def extract_product_price_info(self, ADVER_ID_LIST, Table_name):
         self.connect_db()
         product_property_df_list = []
         size = ADVER_ID_LIST.shape[0]
@@ -87,21 +94,13 @@ class Shop_Data_Extractor :
                 sql_text = text(price_info_sql)
                 product_price_info_df = pd.read_sql(sql_text, self.MariaDB_Engine_Conn)
                 merged_df = pd.merge(self.product_cate_info_df, product_price_info_df,on=['ADVER_ID','PCODE'])
-                product_property_df_list.append(merged_df)
+                merged_df.to_sql(Table_name, con=self.Local_Click_House_Engine, index=False, if_exists='replace')
                 if i % 10 == 0 :
                     print("{0}/{1} : ".format(i,size),ADVER_ID)
-            product_property_df = pd.concat(product_property_df_list)
-            return product_property_df
+            return True
         except :
             self.inner_logger.log("ADVER ID : {0}, Index : {1}/{2}".format(ADVER_ID, i, size),"failed")
             return False
-
-    def product_property_df_to_DB(self,product_property_df, local_clickhouse_id,
-                                    local_clickhouse_password, local_clickhouse_db_name, table_name):
-        self.connect_local_clickhouse_db(local_clickhouse_id,
-                                    local_clickhouse_password, local_clickhouse_db_name)
-        product_property_df.to_sql(table_name, con=self.Local_Click_House_Engine, index=False, if_exists='replace')
-        return True
 
 if __name__ == "__main__":
 
@@ -112,12 +111,15 @@ if __name__ == "__main__":
     # for service
     maria_id = "analysis"
     maria_password = "analysis@2020"
+    click_house_id = "click_house_test1"
+    click_house_password = "0000"
+    click_house_DB = "TEST"
 
-    shop_data_context = Shop_Data_Extractor(maria_id, maria_password)
+    shop_data_context = Shop_Data_Extractor(maria_id, maria_password,click_house_id, click_house_password,
+                                            click_house_DB)
     adver_id_list = shop_data_context.return_adver_id_list()
     print(adver_id_list)
     product_cate_info = shop_data_context.extract_product_cate_info()
     print(shop_data_context.product_cate_info_df)
-    extract_product_price_info = shop_data_context.extract_product_price_info(adver_id_list)
-    shop_data_context.product_property_df_to_DB(extract_product_price_info,"click_house_test1","0000","TEST","PRODUCT_INFO")
+    extract_product_price_info = shop_data_context.extract_product_price_info(adver_id_list,'PRODUCT_INFO')
     print(extract_product_price_info)
