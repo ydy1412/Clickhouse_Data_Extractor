98b67d31d3646e82b5633dd9016f7d47fb41723e
Yang <ydy1412@gmail.com>
2020-10-28 18:15:24 +0900

    train_ctr_model.py added

--- train_ctr_model.py
+++ train_ctr_model.py
@@ -24,6 +24,10 @@ import calendar
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
 
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras import Model
+
 
 import hashlib
 import os
@@ -63,8 +67,9 @@ def load_encoder() :
     return encoder_dict
 
 
-def preprocess_sample_data(data_extract_context, table_name, start_dttm, last_dttm, encoder_dict, data_cnt=100000):
-    sample_data = data_extract_context.Extract_Sample_Log(table_name, start_dttm, last_dttm, targeting='AD',
+def preprocess_sample_data(data_extract_context, table_name, start_dttm, last_dttm, encoder_dict,
+                           data_cnt=100000, click_ratio = 0.1, targeting = 'AD'):
+    sample_data = data_extract_context.Extract_Sample_Log(table_name, start_dttm, last_dttm, targeting=targeting,
                                                           sample_cnt=data_cnt)
     sample_data = sample_data[['STATS_DTTM', 'STATS_HH', 'STATS_MINUTE', 'MEDIA_SCRIPT_NO',
                                'ADVRTS_PRDT_CODE', 'ADVRTS_TP_CODE', 'ADVER_ID',
@@ -96,6 +101,21 @@ def preprocess_sample_data(data_extract_context, table_name, start_dttm, last_dt
     sample_data.dropna(inplace=True)
     sample_data[['WIDTH', 'HEIGHT']] = sample_data[['WIDTH', 'HEIGHT']] / 1000
 
+    click_cnt = sample_data[sample_data['CLICK_YN']==1].shape[0]
+    print(click_cnt)
+    view_cnt = int(click_cnt/click_ratio)
+    print(view_cnt)
+
+    click_data = sample_data[sample_data['CLICK_YN'] == 1]
+    try :
+        view_data = sample_data[sample_data['CLICK_YN']==0].sample(n=view_cnt)
+    except :
+        view_data = sample_data[sample_data['CLICK_YN']==0]
+    print(view_data.shape)
+    subsample_data = pd.concat([view_data,click_data])
+    print(subsample_data.shape)
+    subsample_data = subsample_data.sample(frac = 1)
+
     encoder_dict = load_encoder()
 
     encode_column_list = ['MONTH_CATE', 'WEEK', 'STATS_HH', 'PLTFOM_TP_CODE']
@@ -103,24 +123,36 @@ def preprocess_sample_data(data_extract_context, table_name, start_dttm, last_dt
     encoded_array_list = []
     for column in encode_column_list:
         print(column)
-        encoded_array = encoder_dict[column].transform(sample_data[[column]]).toarray()
+        encoded_array = encoder_dict[column].transform(subsample_data[[column]]).toarray()
         encoded_array_list.append(encoded_array)
 
-    encoded_array_list.append(hash_trick(sample_data[['MEDIA_CATE_INFO']], 20))
-    encoded_array_list.append(hash_trick(sample_data[['SCRIPT_TP_CODE']], 25))
-    encoded_array_list.append(hash_trick(sample_data[['ADVER_MEDIA_HASH_KEY']]))
-    encoded_array_list.append(hash_trick(sample_data[['BROWSER_CODE']], 20))
-    encoded_array_list.append(hash_trick(sample_data[['OS_CODE']], 15))
-    encoded_array_list.append(sample_data[['WIDTH']].values)
-    encoded_array_list.append(sample_data[['HEIGHT']].values)
-    encoded_array_list.append(sample_data[['SIZE_RATIO']].values)
+    encoded_array_list.append(hash_trick(subsample_data[['MEDIA_CATE_INFO']], 20))
+    encoded_array_list.append(hash_trick(subsample_data[['SCRIPT_TP_CODE']], 25))
+    encoded_array_list.append(hash_trick(subsample_data[['ADVER_MEDIA_HASH_KEY']],100))
+    # encoded_array_list.append(hash_trick(input_data[['BROWSER_CODE']], 20))
+    # encoded_array_list.append(hash_trick(input_data[['OS_CODE']], 15))
+    encoded_array_list.append(subsample_data[['WIDTH']].values)
+    encoded_array_list.append(subsample_data[['HEIGHT']].values)
+    encoded_array_list.append(subsample_data[['SIZE_RATIO']].values)
 
     input_data = np.hstack(encoded_array_list)
 
-    output_data = sample_data[['CLICK_YN']].values
-    return input_data, output_data
+    output_data = subsample_data[['CLICK_YN']].values
+    return input_data, np.squeeze(output_data)
 
-def MyModel()
+def Return_NN_Model(input_shape, node_list) :
+    layer_list = []
+    for node_count in node_list :
+        layer_list.append(tf.keras.layers.Dense(node_count, activation='relu'))
+        layer_list.append(tf.keras.layers.Dropout(0.2))
+    layer_list.insert(0,tf.keras.layers.Dense(input_shape,activation='relu'))
+    layer_list.insert(-1,tf.keras.layers.Dense(2,activation='softmax'))
+    model = tf.keras.models.Sequential(layer_list)
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
 
 if __name__ == '__main__' :
     clickhouse_id = "analysis"
@@ -134,17 +166,52 @@ if __name__ == '__main__' :
                                                 local_clickhouse_ip, local_clickhouse_DB_name,
                                                 local_clickhouse_Table_name)
 
-    model = LogisticRegression(penalty='l2')
+    # model = LogisticRegression(penalty='l2')
     encoder_dict = load_encoder()
 
-    for i in range(1):
+    for i in range(1) :
+        vali_input_data, vali_output_data = preprocess_sample_data(test_context, 'CLICK_VIEW_YN_LOG_NEW',
+                                                                   '20201001','20201019', encoder_dict, 100000, 0.0001)
+        vali_input_data, vali_output_data =  vali_input_data.astype(np.float32), vali_output_data.astype(np.float32)
+        print("validation data extracted")
+
+    model = Return_NN_Model(vali_input_data.shape[1], [50, 10, 10])
+
+    checkpoint_path = "training_1/cp.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                     save_weights_only = True,
+                                                     verbose=1)
+
+    for i in range(10):
         print("train sequence {0}".format(i))
         input_data, output_data = preprocess_sample_data(test_context, 'CLICK_VIEW_YN_LOG_NEW',
-                                                         '20201001', '20201010', encoder_dict, 100000)
-        model.fit(input_data, output_data)
+                                                         '20201001', '20201019', encoder_dict, 1000000,0.0001)
+        input_data, output_data  = input_data.astype(np.float32), output_data.astype(np.float32)
+        print(input_data.shape, output_data.shape)
+        model.fit(input_data,
+                  output_data,
+                  epochs=5,
+                  validation_data=(vali_input_data, vali_output_data),
+                  callbacks=[cp_callback])
 
-    test_input_data, test_output_data = preprocess_sample_data(test_context, 'CLICK_VIEW_YN_LOG_NEW', '20201001', '20201010',
-                                                               encoder_dict, 100000)
-    predict_prob_y = model.predict_proba(test_input_data)
-    CTR = predict_prob_y[:, 1] * 100
-    print(CTR)
+    # 훈련된 모델 파라미터 불러오는 방법.
+    # new_model = Return_NN_Model(vali_input_data.shape[1], [50, 10, 10])
+    # new_model.load_weights(checkpoint_path)
+
+
+
+    # print(input_data.shape, output_data.shape)
+    # input_shape = input_data.shape[0]
+    # print(input_shape)
+    #
+    #
+    # model = MyModel(input_shape)
+    # loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+    # optimizer = tf.keras.optimizers.Adam()
+    #
+    # test_input_data, test_output_data = preprocess_sample_data(test_context, 'CLICK_VIEW_YN_LOG_NEW', '20201001', '20201010',
+    #                                                            encoder_dict, 100000)
+    # predict_prob_y = model.predict_proba(test_input_data)
+    # CTR = predict_prob_y[:, 1] * 100
+    # print(CTR)
